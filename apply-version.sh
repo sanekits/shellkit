666c5789daa3e394f6ad88714c32bf965fa2956f
Les Matheson <les.matheson@gmail.com>
2022-08-21 08:50:49 -0400

    Remove `set -x` from apply-version.sh

--- apply-version.sh
+++ apply-version.sh
@@ -30,14 +30,12 @@ update_version_script() {
 }
 
 main() {
-    set -x
     local version=$(cat version)
     local kitname=$(cat bin/Kitname)
     [[ -n $version ]] || die bad version
     [[ -n $kitname ]] || die bad kitname
     [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || die "bad version: \"$version\""
     update_readme_version $kitname $version || die main.3
-    set -x
     update_version_script $kitname $version || die main.4
     true
 }

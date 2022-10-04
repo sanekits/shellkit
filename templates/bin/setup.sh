7348d86b6f0cc87961890091e1da1b9af19feb92
Les Matheson <lmatheson4@bloomberg.net>
2022-10-04 11:11:14 -0400

    Fix read perms on parents of .local/bin/[kitname]

--- templates/bin/setup.sh
+++ templates/bin/setup.sh
@@ -43,7 +43,8 @@ main() {
     # able to traverse dirs and exec scripts, so that a source installation can
     # be replicated to a dest from the same file system (e.g. docker containers,
     # nfs-mounted home nets, etc)
-    command chmod og+rX ${HOME}/.local/bin/${Kitname} -R;
+    command chmod og+rX ${HOME}/.local/bin/${Kitname} -R
+    command chmod og+rX ${HOME}/.local ${HOME}/.local/bin
 }
 
 [[ -z ${sourceMe} ]] && {

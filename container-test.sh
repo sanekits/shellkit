6e5d346063bc3c8d8476d1e013a43a31fba97bc8
LES MATHESON <LMATHESON4@bloomberg.net>
2025-04-17 21:54:59 +0000

    Sync (auto)

--- container-test.sh
+++ container-test.sh
@@ -4,10 +4,7 @@
 #  Starts a container for testing by launching a shellkit-component process.
 #
 #   Example:
-#      Command='echo hello' \
 #        container-test.sh --component shellkit-conformity --keep-shell
-#
-#   - See $ShellkitWorkspace/.devcontainer/shellkit-component.mk
 #
 #   - Mounts :
 #      - temp dir on the host into ~/.local/bin of the container so that
@@ -15,50 +12,31 @@
 #        the host side
 #      - PWD:/workspace:ro
 #
-#   - Env:
-#       Command="command line" # to launch process when container is started, or defaults to shell
-#
 #   - Args:
 #       --component:  # basename of the component, e.g. shellkit-conformity, shellkit-pytest, etc.
 #       --keep-shell,-k:  # Stay in the container shell instead of exiting after command
 
-scriptName="$(readlink -f "$0")"
+scriptName="${scriptName:-$(readlink -f "$0")}"
 #shellcheck disable=2154
 PS4='$( _0=$?; exec 2>/dev/null; realpath -- "${BASH_SOURCE[0]:-?}:${LINENO} ^$_0 ${FUNCNAME[0]:-?}()=>" ) '
 
-setShellkitWorkspace() {
-    local xd=$PWD
-    while [[ $xd != / ]]; do
-        [[ -f ${xd}/.shellkit-workspace ]] && { ShellkitWorkspace=$xd; return; }
-        xd=$(dirname -- "$xd")
-    done
-}
-setShellkitWorkspace
+# setShellkitWorkspace() {
+#     local xd=$PWD
+#     while [[ $xd != / ]]; do
+#         [[ -f ${xd}/.shellkit-workspace ]] && { ShellkitWorkspace=$xd; return; }
+#         xd=$(dirname -- "$xd")
+#     done
+# }
+# setShellkitWorkspace
 KeepShell=false
 
 set -ue
-ComponentDockerMakefile=${ShellkitWorkspace}/.devcontainer/shellkit-component.mk
 Component=
 Read_only_workspace=":ro"
 
 die() {
     builtin echo "ERROR($(basename "${scriptName}")): $*" >&2
     builtin exit 1
-}
-
-stub() {
-    # Print debug output to stderr.  Recommend to call like this:
-    #   stub "${FUNCNAME[0]}.${LINENO}" "$@" "<Put your message here>"
-    #
-    set +u
-    [[ -n $NoStubs ]] && { set -u; return; }
-    [[ -n $__stub_counter ]] && (( __stub_counter++  )) || __stub_counter=1
-    {
-        builtin printf "  <=< STUB(%d:%s)" "$__stub_counter" "$(basename "$scriptName")"
-        builtin printf "[%s] " "$@"
-        builtin printf " >=> \n"
-    } >&2
-    set -u
 }
 
 main() {
@@ -84,7 +62,6 @@ main() {
     # Create+launch the container:
 	local volumes="-v ${PWD}:/workspace${Read_only_workspace}  \
         -v ${tmpLocalBin}:/home/vscode/.local/bin  \
-        -v ${ShellkitWorkspace}:/shellkit-workspace"
     echo "Command=${Command}"
     echo "Component=${Component}"
     make -f "${ComponentDockerMakefile}" \
